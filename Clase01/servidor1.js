971187f340a934ea81c318f7cade528145cb7826
Diego Serrano <diegojserrano@gmail.com>
2020-08-24 20:24:57 -0300

    Ejercicios y resumen clase 2

--- Clase01/servidor1.js
+++ Clase01/servidor1.js
@@ -3,11 +3,27 @@ const { read } = require('fs')
 
 
 let visitas = 0
+let nombres = []
 
 // createServer requiere un parámetro
 // ese parámetro debe ser una función llamada callback
 http.createServer((req, res) => {
-    if (req.url == "/contar") {
+    if (req.url.startsWith("/agregar/")) {
+        let nombre = req.url.split("/")[2]
+        nombres.push(nombre)
+        req.url
+        res.end("Nombre " + nombre + " agregado correctamente")
+    }
+
+    
+    else if (req.url == "/listar") {
+        let table = "<table border=1>"
+        for (let i = 0; i < nombres.length; i++) 
+            table += "<tr><td>" + nombres[i] + "</td></tr>"
+        table += "</table>"
+        res.end(table)
+    }
+    else if (req.url == "/contar") {
         visitas++
         res.end("contado correctamente")
     }
